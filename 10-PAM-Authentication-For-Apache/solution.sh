03308af68b96b4d40319d66dcfb97a0757efd434
Hazunanafaru <hazunanafaru@gmail.com>
2022-09-01 20:39:38 +0700

    feat(task-10): first try of this task

---
+++ 10-PAM-Authentication-For-Apache/solution.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# We have a requirement where we want to password protect a directory in the Apache web server document root.
+# We want to password protect http://<website-url>:<apache_port>/protected URL as per the following requirements
+# (you can use any website-url for it like localhost since there are no such specific requirements as of now).
+# Setup the same on App server 3 as per below mentioned requirements:
+#  a. We want to use basic authentication.
+#  b. We do not want to use htpasswd file based authentication.
+#     Instead, we want to use PAM authentication, i.e Basic Auth + PAM
+#     so that we can authenticate with a Linux user.
+#  c. We already have a user ammar with password YchZHRcLkL which you need to provide access to.
+#  d. You can access the website using APP button on the top bar.
+
+# Login as root
+sudo su -
+
+# Check if the server is PAM aware
+ldd /usr/sbin/sshd | grep libpam.so
